a9b9ef4eed7309a64bce989c3e3c642f0a35a074
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-07 09:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-// Complete the js code
-function Car(make, model) {}
+// Car constructor function
+function Car(make, model) {
+    this.make = make; // Initialize make property
+    this.model = model; // Initialize model property
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add getMakeModel method to Car's prototype
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`; // Return make and model as a string
+};
+
+// SportsCar constructor function
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model); // Call the Car constructor to initialize make and model
+    this.topSpeed = topSpeed; // Initialize topSpeed property
+}
+
+// Inherit from Car's prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar; // Set the constructor property to SportsCar
+
+// Add getTopSpeed method to SportsCar's prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed; // Return the top speed
+};
 
 // Do not change the code below
 window.Car = Car;
